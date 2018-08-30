0602a4491cf576d287abf70c35335c4ec05f5802
Travis Job <travis@iov.invalid>
2018-08-30 11:06:08 +0000

    Update docs

---
+++ latest/iov-faucets/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"128":"Class","512":"Constructor","2048":"Method"},"rows":[{"id":0,"kind":128,"name":"BovFaucet","url":"classes/bovfaucet.html","classes":"tsd-kind-class"},{"id":1,"kind":512,"name":"constructor","url":"classes/bovfaucet.html#constructor","classes":"tsd-kind-constructor tsd-parent-kind-class","parent":"BovFaucet"},{"id":2,"kind":2048,"name":"credit","url":"classes/bovfaucet.html#credit","classes":"tsd-kind-method tsd-parent-kind-class","parent":"BovFaucet"}]};
